b7a7304eef3fe321125cc8780625f17bf2a94a60
MadBar <madeleinetrollvik@gmail.com>
2017-11-25 21:28:55 +0100

    Implementing his test divition with 0
    Does all zero tests in same test case!

--- src/test/java/DivitionTest.java
+++ src/test/java/DivitionTest.java
@@ -95,12 +95,19 @@ public class DivitionTest {
 
 	@Test
 	public void testDivZeros() {
-		double firstNumber = 1;
+		double firstNumber = 0;
 		double secondNumber = 0;
-		double result = firstNumber / secondNumber;
-		double resultMethod = calculator.divide(firstNumber, secondNumber);
 
-		// Works but this should be handled in calculator to make the system safe and
+		// Mine 02:
+		// double result = firstNumber / secondNumber;
+		// His:
+		double result = -0.123456789;
+
+		// Mine 02:
+		// double resultMethod = calculator.divide(firstNumber, secondNumber);
+
+		// Mine 01: Works but this should be handled in calculator to make the system
+		// safe and
 		// not just the test
 		// if (firstNumber == 0.0 || secondNumber == 0.0) {
 		//
@@ -118,17 +125,52 @@ public class DivitionTest {
 		// Math.round(result), 1);
 		// }
 
-		// Test of handling 0-erros in Cal* This prints out Cal method sysout twice
-		// since its also called in the assert.. Don't like it.. How to fix? 
-		if (resultMethod == -0.123456789) {
+		// // Mine 02: Test of handling 0-erros in Cal* This prints out Cal method
+		// sysout twice
+		// // since its also called in the assert.. Don't like it.. How to fix?
+		// if (resultMethod == -0.123456789) {
+		//
+		// LOG.info("Testing the method divide zeros with: " + firstNumber + " and " +
+		// secondNumber
+		// + " . You can't devide by zero!!");
+		// assertEquals(Math.round(calculator.divide(firstNumber, secondNumber)),
+		// Math.round(-0.123456789), 1);
+		// } else {
+		// LOG.info("Testing the method divide zeros with: " + firstNumber + " and " +
+		// secondNumber
+		// + " . Result of those: " + result + ". Result from Method is: " +
+		// resultMethod);
+		// assertEquals(Math.round(calculator.divide(firstNumber, secondNumber)),
+		// Math.round(result), 1);
+		// }
 
-			LOG.info("Testing the method divide zeros with: " + firstNumber + " and " + secondNumber
-					+ " . You can't devide by zero!!");
-			assertEquals(Math.round(calculator.divide(firstNumber, secondNumber)), Math.round(-0.123456789), 1);
-		} else {
-			LOG.info("Testing the method divide zeros with: " + firstNumber + " and " + secondNumber
-					+ " . Result of those: " + result + ". Result from Method is: " + resultMethod);
+		// His:
+		// *He does all tests in the same testcase, after eachother. Smart. Just giving
+		// new values to first and second nr, each test.
+		//*He has already decided that the method should return the faulty value..? Why?
+		// Aah, cause he only want to have a test for that maybe.. Nope. Sees like the
+		// first log should have a note saying: "Testing divide 0 and 0".
+		LOG.info("Testing the method divide zeros with: " + firstNumber + " and " + secondNumber);
+		assertEquals(Math.round(calculator.divide(firstNumber, secondNumber)), Math.round(result), 1);
+
+		for (int i = 0; i < 1; i++) {
+
+			// Testing the case of something random / zero
+			firstNumber = Double.valueOf(df.format(random.nextDouble() * 10));
+			secondNumber = 0;
+			result = -0.123456789;
+
+			LOG.info("Testing the method divide with: " + firstNumber + " and " + secondNumber);
 			assertEquals(Math.round(calculator.divide(firstNumber, secondNumber)), Math.round(result), 1);
+
+			// Testing the case of zero / something random
+			firstNumber = 0;
+			secondNumber = Double.valueOf(df.format(random.nextDouble() * 10));
+			result = firstNumber / secondNumber;
+
+			LOG.info("Testing the method divide with: " + firstNumber + " and " + secondNumber);
+			assertEquals(Math.round(calculator.divide(firstNumber, secondNumber)), Math.round(result), 1);
+
 		}
 
 	}
